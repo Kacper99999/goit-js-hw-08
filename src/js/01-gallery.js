636248355348bcb47c1a86b8b6f155b8e7decf0d
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-04-08 16:40:21 +0200

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,52 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+
+function createImages(){
+  for(let i = 0; i < galleryItems.length; i++){
+      //Create list elements------------------------------
+      const liTag = document.createElement("li");
+      liTag.classList.add("gallery__item");
+
+      //Create link elements------------------------------
+      const linkTag = document.createElement("a");
+      linkTag.classList.add("gallery__link");
+      linkTag.href = galleryItems[i].original;
+
+      //Create img elements------------------------------
+      const imageTag = document.createElement("img");
+      imageTag.classList.add("gallery__image");
+      imageTag.src = galleryItems[i].preview;
+      imageTag.alt = galleryItems[i].description;
+
+      //Including HTML---------------------------------------------------
+      gallery.appendChild(liTag);
+      liTag.appendChild(linkTag);
+      linkTag.appendChild(imageTag);
+
+}
+};
+
+
+let lightbox = null;
+
+function selectImage(event){
+  event.preventDefault();
+  if(!lightbox){
+    lightbox = new SimpleLightbox('.gallery a', {
+      captions: true, 
+      captionType: 'attr', 
+      captionDelay: 250, 
+      captionsData: 'alt' 
+  });
+ }
+};
+
+createImages();
+
+
+gallery.addEventListener("click", selectImage);
