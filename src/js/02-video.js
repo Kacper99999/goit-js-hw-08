205bc5cfc342551e51ebd2028e646e52aba220e2
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-04-08 18:15:38 +0200

    Update video02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,5 +5,5 @@ const iframe = document.querySelector("iframe");
 const player = new Player(iframe);
 
 player.on("pause", (event) => {
-    localStorage.setItem("klucz",event.duration);
+    localStorage.setItem("klucz",event.seconds);
 });
