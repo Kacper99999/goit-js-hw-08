56b658d7eec326312de30815f101cef3d1bb3012
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-04-09 18:59:56 +0200

    Update feedback03.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,6 @@ import { throttle  } from "lodash";
 
 const form = document.querySelector("form");
 
-
 const storage = JSON.parse(localStorage.getItem("feedback-form-state"));
 form.elements.email.value = storage.email;
 form.elements.message.value = storage.message;
@@ -12,8 +11,8 @@ const save = throttle((event) => {
         storage[name] = value;
             localStorage.setItem("feedback-form-state", JSON.stringify(storage));
     },500);
-form.addEventListener("input", save);
 
+form.addEventListener("input", save);
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
