a2de80d9e7869e12489744f5a84fe35bd7229b80
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-04-09 18:49:39 +0200

    Video02.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,32 @@
 import Player from '@vimeo/player';
-import { throttle } from 'lodash';
+import { every, throttle } from 'lodash';
 
 const iframe = document.querySelector("iframe");
 const player = new Player(iframe);
 
 
+
+
+
 const throttled = throttle((event) => {
-    const currentTime = event.seconds
-    player.setCurrentTime(currentTime).then(() =>{
-    }).catch(function(error) {
+    const currentTime = event.seconds;
+    localStorage.setItem("videoplayer-current-time", currentTime);
+}, 1000);
+
+player.on("timeupdate", throttled);
+
+const storedTime = localStorage.getItem("videoplayer-current-time");
+
+    player.setCurrentTime(storedTime).then()
+    .catch(error => {
         switch (error.name) {
             case 'RangeError':
-                break;
 
+                break;
             default:
+
                 break;
         }
-})
-},1000)
-player.on("timeupdate",throttled);
+    });
 
 
