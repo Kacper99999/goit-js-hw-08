8b554ad1ece608784f5c456f76c2c79f3d3c0f75
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2024-04-09 16:51:44 +0200

    Feedback03.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+import { every, throttle  } from "lodash";
 
+const form = document.querySelector("form");
+
+const key = "feedback-form-state";
+const zmienna = JSON.parse(localStorage.getItem(key)) || {};
+form.elements.email.value = zmienna.value;
+form.elements.message.value = zmienna.message;
+
+form.addEventListener("input", (event) => {
+    const {name, value} = event.target;
+    zmienna[name] = value;
+    localStorage.setItem(key, JSON.stringify(zmienna));
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    localStorage.removeItem(key)
+    form.reset();
+});
